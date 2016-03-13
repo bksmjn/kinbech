ccd8b6c29e101aaacf06ce47225ef99dfa9c6f26
bikesh_maharjan <bksmjn@gmail.com>
2016-03-13 14:42:25 -0500

    Added Controller and Repository

---
+++ kinbech/src/main/java/com/codebhatti/kinbech/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.codebhatti.kinbech.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.codebhatti.kinbech.domain.User;
+import com.codebhatti.kinbech.service.UserService;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+	
+	@Autowired
+	private UserService  userService;
+	
+	@RequestMapping(value="/add",method=RequestMethod.GET)
+	public String getUserAddForm(@ModelAttribute("user")User user){
+		return "UserAdd";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public void addUser(@ModelAttribute("user")User user,BindingResult result){
+		try{
+			this.userService.addUser(user);
+		}catch(IllegalArgumentException ex){
+			throw new IllegalArgumentException(ex.getMessage());
+		}
+		
+	}
+
+	public UserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	
+
+}
